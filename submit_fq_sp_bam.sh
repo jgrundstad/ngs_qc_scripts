7493c00c6b567daf85e6d96d912450a1bec9ffa9
Jason Grundstad <jgrundstad@gmail.com>
2014-05-09 15:01:44 -0500

    added pbs runner, calls .sh to thread conversion

--- submit_fq_sp_bam.sh
+++ submit_fq_sp_bam.sh
@@ -2,6 +2,7 @@
 
 DIR=$1
 cd $DIR
-
+PWD=pwd
+echo "$PWD"
 #2013-2117_140318_SN673_0216_BC3YHHACXX_3_1_sequence.txt.gz
 ls *_1_sequence.txt.gz | cut -f1-6 -d "_" | xargs -iSTUB -n 1 -P 12 /lustre/beagle/jgrundst/ngs_qc_scripts/fastq_sp_bam.sh STUB_1_sequence.txt.gz STUB_2_sequence.txt.gz
