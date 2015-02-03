08e24b70857250ac9cf90a517dba9eaac45b3585
Jason Grundstad <jgrundstad@gmail.com>
2015-02-03 11:00:24 -0600

    new nodes, more cores, edited

--- submit_fq_sp_bam.sh
+++ submit_fq_sp_bam.sh
@@ -11,7 +11,8 @@ INPUT_STRING=$(ls *bam | parallel echo "I={}" | paste -s -d " ")
 
 mkdir -p final_merge_tmp
 
-java -Xmx8g -jar $PICARD_DIR/MergeSamFiles.jar TMP_DIR=final_merge_tmp $INPUT_STRING O=$DIR.bam SO=unsorted ASSUME_SORTED=false VERBOSITY=DEBUG USE_THREADING=true VALIDATION_STRINGENCY=LENIENT > $DIR.final.merge.log 2>&1
+#java -Xmx8g -jar $PICARD_DIR/MergeSamFiles.jar TMP_DIR=final_merge_tmp $INPUT_STRING O=$DIR.bam SO=unsorted ASSUME_SORTED=false VERBOSITY=DEBUG USE_THREADING=true VALIDATION_STRINGENCY=LENIENT > $DIR.final.merge.log 2>&1
+/lustre/beagle/cbandlam/libs/novosort/novosort --threads 20 --ram 2048M --tmpcompression 6 --tmpdir final_merge_tmp --assumesorted --output $DIR.bam *_[1-8].bam > $DIR.final.novosort.merge.log 2>&1
 
 rm -rf final_merge_tmp
 
@@ -19,4 +20,4 @@ chmod 644 $DIR.bam
 
 samtools flagstat $DIR.bam > $DIR.bam.flagstats
 
-#rm *_[1-8].bam
+rm *_[1-8].bam
