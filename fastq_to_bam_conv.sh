e3ecfb0dc1fd8761fc75756f413c2708b42f7fe8
Jason Grundstad <jgrundstad@gmail.com>
2015-05-05 09:33:57 -0500

    removed fifos, went back to disk IO

--- fastq_to_bam_conv.sh
+++ fastq_to_bam_conv.sh
@@ -51,21 +51,23 @@ PICARD_TEMP="$SAMPLE.picard_temp"
 # required for picard ops
 mkdir -p $PICARD_TEMP
 
-mkfifo $FQ1.conv.fifo
-mkfifo $FQ2.conv.fifo
+FQ1_CONV=$FQ1.conv.gz
+FQ2_CONV=$FQ2.conv.gz
 
-zcat $FQ1 | $SCRIPT_DIR/convert_stream_illumina-sanger.pl > $FQ1.conv.fifo &
-zcat $FQ2 | $SCRIPT_DIR/convert_stream_illumina-sanger.pl > $FQ2.conv.fifo &
+ls $FQ1 $FQ2 | xargs -IFILE -P 2 -n 1 bash -c \
+	"zcat FILE | $SCRIPT_DIR/convert_stream_illumina-sanger.pl | gzip -c - > FILE.conv.gz"
 
+# pause to let MDS catch up
+sleep 60
 
 # convert pair
-java -Xmx2g -jar $PICARD_DIR/FastqToSam.jar TMP_DIR=$PICARD_TEMP FASTQ=$FQ1.conv.fifo FASTQ2=$FQ2.conv.fifo OUTPUT=$FINAL_BAM \
+java -Xmx2g -jar $PICARD_DIR/FastqToSam.jar TMP_DIR=$PICARD_TEMP FASTQ=$FQ1_CONV FASTQ2=$FQ2_CONV OUTPUT=$FINAL_BAM \
 	READ_GROUP_NAME=$SAMPLE SAMPLE_NAME=$ID LIBRARY_NAME=$ID PLATFORM=illumina QUALITY_FORMAT=Standard \
 	SORT_ORDER=unsorted VALIDATION_STRINGENCY=LENIENT VERBOSITY=DEBUG > $SAMPLE.convert.log 2>&1
 
 # cleanup
 rm -rf $PICARD_TEMP
-rm *.conv.fifo
+rm $FQ1_CONV $FQ2_CONV
 
 # fix mode
 chmod 644 $ID*
